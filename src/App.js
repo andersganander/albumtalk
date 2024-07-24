69fddf79b20b16cf15530cf03ba5265878d94517
Anders Ganander <anders.ganander@gmail.com>
2024-07-24 19:14:15 +0000

    USER STORY: Create account #5

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
 
 
 import AlbumPage from "./pages/AlbumPage";
 import AlbumsPage from "./pages/AlbumsPage";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -17,7 +19,7 @@ function App() {
           <Route exact path="/albums" render={() => <h1>Albums</h1>} />
           <Route exact path="/reviews" render={() => <h1>Reviews</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/albums/:id" render={() => <AlbumPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
