df9849f2e4da45fc8272cd8aa036f10485e3b1e0
Anders Ganander <anders.ganander@gmail.com>
2024-07-27 07:32:03 +0000

    Adjust navbar for logged in/logged out #41

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,40 @@ import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const loggedIn = <>{currentUser?.username}</>;
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const loggedIn = (
+    <>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        SIGNOUT
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
+  
   const loggedOut = (
     <>
       <NavLink
