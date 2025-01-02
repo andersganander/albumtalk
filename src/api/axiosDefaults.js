896473d581308f85d6cf33668cc1d5f30f4c7bf3
Anders Ganander <anders.ganander@gmail.com>
2025-01-02 20:58:29 +0000

    Revert baseURL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//axios.defaults.baseURL = "https://albumtalk-api-9fb1bb849439.herokuapp.com/";
-axios.defaults.baseURL = "https://album-talk-68a3b5c7423a.herokuapp.com/";
+axios.defaults.baseURL = "https://albumtalk-api-9fb1bb849439.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
