fd963e14108c8b9c765f0f51842ce0bdd088fc6e
Anders Ganander <anders.ganander@gmail.com>
2025-01-06 19:13:22 +0000

    Update baseURL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://albumtalk-api-9fb1bb849439.herokuapp.com/";
+//axios.defaults.baseURL = "https://albumtalk-api-9fb1bb849439.herokuapp.com/";
+axios.defaults.baseURL = "api.skivsnack.se/";
+
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
